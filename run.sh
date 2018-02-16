6615d53fdc9ffa5b1b72ac9f9a4d169663f1c5c0
moonmoonbird <lomoonmoonbird@gmail.com>
2018-02-16 12:15:13 +0800

    add shell scripts

---
+++ run.sh
@@ -0,0 +1,2 @@
+exec /home/installation/python363/bin/python3.6 /home/jinpeng/cron/moonmoonbird-godaddy-ddns/moonmoonbird_godaddy_ddns.py %/home/jinpeng/cron/moonmoonbird-godaddy-ddns/config.txt
+exec /home/installation/python363/bin/python3.6 /home/jinpeng/cron/moonmoonbird-godaddy-ddns/moonmoonbird_godaddy_ddns.py %/home/jinpeng/cron/moonmoonbird-godaddy-ddns/config_git.txt
