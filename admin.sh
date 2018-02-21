663bb76d9d1a0746c60b93dc002109982dd35f98
moonmoonbird <lomoonmoonbird@gmail.com>
2018-02-21 10:14:51 +0800

    change filename

--- admin.sh
+++ admin.sh
@@ -1,2 +1,2 @@
-exec /home/installation/python363/bin/python3.6 /home/jinpeng/cron/moonmoonbird-godaddy-ddns/moonmoonbird_godaddy_ddns.py %/home/jinpeng/cron/moonmoonbird-godaddy-ddns/admin_config.txt
+exec /home/installation/python363/bin/python3.6 /home/jinpeng/cron/moonmoonbird-godaddy-ddns/moonmoonbird_godaddy_ddns.py %/home/jinpeng/cron/moonmoonbird-godaddy-ddns/config_adin.txt
  
