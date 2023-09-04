f437d9f2f8ed31d4cc9c8c3db341155c92b29d7f
moonmoonbird <lomoonmoonbird@gmail.com>
2023-09-04 18:18:23 +0800

    更新脚本

--- moonmoonbird_godaddy_ddns.py
+++ moonmoonbird_godaddy_ddns.py
@@ -4,36 +4,6 @@
 #
 # Update GoDaddy DNS "A" Record.
 #
-# usage: godaddy_ddns.py [-h] [--version] [--ip IP] [--key KEY]
-#                        [--secret SECRET] [--ttl TTL]
-#                        hostname
-#
-# positional arguments:
-#   hostname         DNS fully-qualified host name with an 'A' record
-#
-# optional arguments:
-#   -h, --help       show this help message and exit
-#   --version        show program's version number and exit
-#   --ip IP          DNS Address (defaults to public WAN address from http://ipv4.icanhazip.com/)
-#   --key KEY        GoDaddy production key
-#   --secret SECRET  GoDaddy production secret
-#   --ttl TTL        DNS TTL.
-#
-# GoDaddy customers can obtain values for the KEY and SECRET arguments by creating a production key at 
-# https://developer.godaddy.com/keys/.  
-# 
-# Note that command line arguments may be specified in a FILE, one to a line, by instead giving
-# the argument "%FILE".  For security reasons, it is particularly recommended to supply the 
-# KEY and SECRET arguments in such a file, rather than directly on the command line:
-#
-# Create a file named, e.g., `godaddy-ddns.config` with the content:
-#   MY.FULLY.QUALIFIED.HOSTNAME.COM
-#   --key
-#   MY-KEY-FROM-GODADDY
-#   --secret
-#   MY-SECRET-FROM-GODADDY
-#
-# Then just invoke `godaddy-ddns %godaddy-ddns.config`
 
 import argparse
 import requests
